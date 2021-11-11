b9852f2fb980fd47e7b606d47d93f9f60ce98198
Denzel Sapatin <dknsapatin@gmail.com>
2021-11-11 11:09:57 -0800

    Added image brain image onto logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Tilt from "react-tilt";
+import brain from "./brain.png";
 import "./Logo.css";
 
 const Logo = () => {
@@ -7,10 +8,19 @@ const Logo = () => {
     <div className="ma4 mt0">
       <Tilt
         className="Tilt br2 shadow-2"
-        options={{ max: 75 }}
+        options={{
+          max: 35,
+          reverse: false,
+          speed: 200,
+          easing: "cubic-bezier(.03,.98,.52,.99)",
+          reverse: false,
+        }}
         style={{ height: 150, width: 150 }}
       >
-        <div className="Tilt-inner"> 👽 </div>
+        <div className="Tilt-inner">
+          {" "}
+          <img alt="Logo" src={brain} />{" "}
+        </div>
       </Tilt>
     </div>
   );
