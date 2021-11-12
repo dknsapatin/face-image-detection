e07dc3d548ba0f332ac2a2f31af137d8cd4b67fa
Denzel Sapatin <dknsapatin@gmail.com>
2021-11-12 11:27:18 -0800

    Customized ImageLinkForm with background and button layout

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "./ImageLinkForm.css";
 
 const ImageLinkForm = () => {
   return (
     <div>
       <p className="f3">{"This will detect faces. Give me a TRY! "}</p>
       <div className="center">
-        <div className="pa5 br7 shadow-5">
+        <div className=" form center pa4 br5 shadow-5">
           <input className="f4 pa2 w-90 center" type="tex" />
           <button className="w-40 grow f5 link ph3 pv2 dib white bg-light-red">
             DETECT
