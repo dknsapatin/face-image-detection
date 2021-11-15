ae025307ce763d263c25fe34f81d8316463b3062
dknsapatin <dknsapatin@gmail.com>
2021-11-14 23:22:59 -0500

    fixed output for rank

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,7 +2,7 @@ import React from "react";
 const Rank = () => {
   return (
     <div>
-      <div className="white f2">{"Denzel, your current rank is..."}</div>
+      <div className="white f2">{"(Username), your current rank is:"}</div>
       <div className="white f1">{"#5"}</div>
     </div>
   );
