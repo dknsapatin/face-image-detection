004d66d44ea824fdfc12424f3321fa8f72ae283d
Denzel Sapatin <dknsapatin@gmail.com>
2021-11-16 15:17:28 -0500

    Added the Clarifai API to detect faces on images. | Only have it detect that there is an image present

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import react, { Component } from "react";
 import "./App.css";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
 import Rank from "./components/Rank/Rank";
 import Particles from "react-particles-js";
 
+// Clarifai API
+const app = new Clarifai.App({
+  apiKey: "c7fd46dde6274cf9a6daaf39cd8305b3",
+});
+// Particles API
 const particlesOptions = {
   particles: {
     line_linked: {
@@ -23,6 +29,8 @@ const particlesOptions = {
     },
   },
 };
+
+//App starts here
 class App extends Component {
   constructor() {
     super();
@@ -30,13 +38,24 @@ class App extends Component {
       input: "",
     };
   }
-
+  //on the input line
   onInputChange = (event) => {
     console.log(event.target.value);
   };
-
+  //Detect Submit Button
   onButtonSubmit = (event) => {
     console.log("click");
+    app.models
+      .predict(
+        "a403429f2ddf4b49b307e318f00e528b",
+        "https://samples.clarifai.com/face-det.jpg"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {}
+      );
   };
   render() {
     return (
